7e45a770e228a3f7cf440dee6bc566f09e0d7875
Nathanael Abel <75358980+nathanaelabel@users.noreply.github.com>
2025-06-28 09:50:24 +0700

    feat: add seeders for dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserSeeder::class);
+        $this->call(NationalRevenueSeeder::class);
+        $this->call(OverdueAccountsReceivableSeeder::class);
 
     }
 }
