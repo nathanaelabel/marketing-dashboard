548449daf5721b26d07f1cea99b00bc68bb61c5d
Nathanael Abel <75358980+nathanaelabel@users.noreply.github.com>
2025-08-19 17:16:56 +0700

    refactor: move accounts receivable js to resource

--- resources/js/dashboard/accounts-receivable.js
+++ resources/js/dashboard/accounts-receivable.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         maintainAspectRatio: false,
                         plugins: {
                             legend: {
-                                position: 'top',
+                                position: 'top'
                             },
                             tooltip: {
                                 mode: 'index',
