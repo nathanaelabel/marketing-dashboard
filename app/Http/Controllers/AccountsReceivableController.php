933d31aecabfb2ad09f5cd270163e550b4c93d89
Nathan <nathancodelab@gmail.com>
2025-11-06 09:47:43 +0700

    fix: update ar query

--- app/Http/Controllers/AccountsReceivableController.php
+++ app/Http/Controllers/AccountsReceivableController.php
@@ -35,46 +35,8 @@ class AccountsReceivableController extends Controller
             'pgsql_trg',
         ];
 
-        // Use CTE for better performance - pre-calculate payments in one pass
+        // Use correlated subquery to calculate payment per invoice (matching original query logic)
         $sql = "
-        WITH payment_summary AS (
-            SELECT
-                alocln.c_invoice_id,
-                SUM(alocln.amount + alocln.writeoffamt + alocln.discountamt) as bayar
-            FROM c_allocationline alocln
-            INNER JOIN c_allocationhdr alochdr ON alocln.c_allocationhdr_id = alochdr.c_allocationhdr_id
-            WHERE alochdr.docstatus IN ('CO', 'IN')
-                AND alochdr.ad_client_id = 1000001
-                AND alochdr.datetrx <= ?
-            GROUP BY alocln.c_invoice_id
-        ),
-        invoice_data AS (
-            SELECT
-                inv.c_invoice_id,
-                inv.totallines,
-                inv.dateinvoiced,
-                org.name as branch_name,
-                COALESCE(ps.bayar, 0) as bayar,
-                CASE
-                    WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NDC') THEN 1
-                    ELSE -1
-                END as pengali,
-                (CURRENT_DATE - inv.dateinvoiced::date) as age
-            FROM c_invoice inv
-            INNER JOIN c_bpartner bp ON inv.c_bpartner_id = bp.c_bpartner_id
-            INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-            LEFT JOIN payment_summary ps ON ps.c_invoice_id = inv.c_invoice_id
-            WHERE SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NCC', 'CNC', 'NDC')
-                AND inv.isactive = 'Y'
-                AND inv.ad_client_id = 1000001
-                AND bp.isactive = 'Y'
-                AND inv.issotrx = 'Y'
-                AND inv.docstatus IN ('CO', 'CL')
-                AND bp.iscustomer = 'Y'
-                AND inv.dateinvoiced <= ?
-                AND inv.c_bpartner_id IS NOT NULL
-                AND inv.totallines IS NOT NULL
-        )
         SELECT
             branch_name,
             SUM(CASE WHEN age >= 1 AND age <= 30 AND (totallines - (bayar * pengali)) <> 0
@@ -87,7 +49,39 @@ class AccountsReceivableController extends Controller
                 THEN (totallines - (bayar * pengali)) ELSE 0 END) as overdue_90_plus,
             SUM(CASE WHEN age >= 1 AND (totallines - (bayar * pengali)) <> 0
                 THEN (totallines - (bayar * pengali)) ELSE 0 END) as total_overdue
-        FROM invoice_data
+        FROM (
+            SELECT
+                inv.totallines,
+                org.name as branch_name,
+                -- Correlated subquery to get payment per invoice
+                (
+                    SELECT COALESCE(SUM(alocln.amount + alocln.writeoffamt + alocln.discountamt), 0)
+                    FROM c_allocationline alocln
+                    INNER JOIN c_allocationhdr alochdr ON alocln.c_allocationhdr_id = alochdr.c_allocationhdr_id
+                    WHERE alocln.c_invoice_id = inv.c_invoice_id
+                        AND alochdr.docstatus IN ('CO', 'IN')
+                        AND alochdr.ad_client_id = 1000001
+                        AND alochdr.datetrx <= ?
+                ) as bayar,
+                CASE
+                    WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NDC') THEN 1
+                    ELSE -1
+                END as pengali,
+                (CURRENT_DATE - inv.dateinvoiced::date) as age
+            FROM c_invoice inv
+            INNER JOIN c_bpartner bp ON inv.c_bpartner_id = bp.c_bpartner_id
+            INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
+            WHERE SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NCC', 'CNC', 'NDC')
+                AND inv.isactive = 'Y'
+                AND inv.ad_client_id = 1000001
+                AND bp.isactive = 'Y'
+                AND inv.issotrx = 'Y'
+                AND inv.docstatus IN ('CO', 'CL')
+                AND bp.iscustomer = 'Y'
+                AND inv.dateinvoiced <= ?
+                AND inv.c_bpartner_id IS NOT NULL
+                AND inv.totallines IS NOT NULL
+        ) as source
         WHERE (totallines - (bayar * pengali)) <> 0
             AND age >= 1
         GROUP BY branch_name
@@ -146,46 +140,8 @@ class AccountsReceivableController extends Controller
             'pgsql_trg',
         ];
 
-        // Use CTE for better performance - pre-calculate payments in one pass
+        // Use correlated subquery to calculate payment per invoice (matching original query logic)
         $sql = "
-        WITH payment_summary AS (
-            SELECT
-                alocln.c_invoice_id,
-                SUM(alocln.amount + alocln.writeoffamt + alocln.discountamt) as bayar
-            FROM c_allocationline alocln
-            INNER JOIN c_allocationhdr alochdr ON alocln.c_allocationhdr_id = alochdr.c_allocationhdr_id
-            WHERE alochdr.docstatus IN ('CO', 'IN')
-                AND alochdr.ad_client_id = 1000001
-                AND alochdr.datetrx <= ?
-            GROUP BY alocln.c_invoice_id
-        ),
-        invoice_data AS (
-            SELECT
-                inv.c_invoice_id,
-                inv.totallines,
-                inv.dateinvoiced,
-                org.name as branch_name,
-                COALESCE(ps.bayar, 0) as bayar,
-                CASE
-                    WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NDC') THEN 1
-                    ELSE -1
-                END as pengali,
-                (CURRENT_DATE - inv.dateinvoiced::date) as age
-            FROM c_invoice inv
-            INNER JOIN c_bpartner bp ON inv.c_bpartner_id = bp.c_bpartner_id
-            INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-            LEFT JOIN payment_summary ps ON ps.c_invoice_id = inv.c_invoice_id
-            WHERE SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NCC', 'CNC', 'NDC')
-                AND inv.isactive = 'Y'
-                AND inv.ad_client_id = 1000001
-                AND bp.isactive = 'Y'
-                AND inv.issotrx = 'Y'
-                AND inv.docstatus IN ('CO', 'CL')
-                AND bp.iscustomer = 'Y'
-                AND inv.dateinvoiced <= ?
-                AND inv.c_bpartner_id IS NOT NULL
-                AND inv.totallines IS NOT NULL
-        )
         SELECT
             branch_name,
             SUM(CASE WHEN age >= 1 AND age <= 30 AND (totallines - (bayar * pengali)) <> 0
@@ -198,7 +154,39 @@ class AccountsReceivableController extends Controller
                 THEN (totallines - (bayar * pengali)) ELSE 0 END) as overdue_90_plus,
             SUM(CASE WHEN age >= 1 AND (totallines - (bayar * pengali)) <> 0
                 THEN (totallines - (bayar * pengali)) ELSE 0 END) as total_overdue
-        FROM invoice_data
+        FROM (
+            SELECT
+                inv.totallines,
+                org.name as branch_name,
+                -- Correlated subquery to get payment per invoice
+                (
+                    SELECT COALESCE(SUM(alocln.amount + alocln.writeoffamt + alocln.discountamt), 0)
+                    FROM c_allocationline alocln
+                    INNER JOIN c_allocationhdr alochdr ON alocln.c_allocationhdr_id = alochdr.c_allocationhdr_id
+                    WHERE alocln.c_invoice_id = inv.c_invoice_id
+                        AND alochdr.docstatus IN ('CO', 'IN')
+                        AND alochdr.ad_client_id = 1000001
+                        AND alochdr.datetrx <= ?
+                ) as bayar,
+                CASE
+                    WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NDC') THEN 1
+                    ELSE -1
+                END as pengali,
+                (CURRENT_DATE - inv.dateinvoiced::date) as age
+            FROM c_invoice inv
+            INNER JOIN c_bpartner bp ON inv.c_bpartner_id = bp.c_bpartner_id
+            INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
+            WHERE SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NCC', 'CNC', 'NDC')
+                AND inv.isactive = 'Y'
+                AND inv.ad_client_id = 1000001
+                AND bp.isactive = 'Y'
+                AND inv.issotrx = 'Y'
+                AND inv.docstatus IN ('CO', 'CL')
+                AND bp.iscustomer = 'Y'
+                AND inv.dateinvoiced <= ?
+                AND inv.c_bpartner_id IS NOT NULL
+                AND inv.totallines IS NOT NULL
+        ) as source
         WHERE (totallines - (bayar * pengali)) <> 0
             AND age >= 1
         GROUP BY branch_name
@@ -392,46 +380,8 @@ class AccountsReceivableController extends Controller
             'pgsql_trg',
         ];
 
-        // Use CTE for better performance - pre-calculate payments in one pass
+        // Use correlated subquery to calculate payment per invoice (matching original query logic)
         $sql = "
-        WITH payment_summary AS (
-            SELECT
-                alocln.c_invoice_id,
-                SUM(alocln.amount + alocln.writeoffamt + alocln.discountamt) as bayar
-            FROM c_allocationline alocln
-            INNER JOIN c_allocationhdr alochdr ON alocln.c_allocationhdr_id = alochdr.c_allocationhdr_id
-            WHERE alochdr.docstatus IN ('CO', 'IN')
-                AND alochdr.ad_client_id = 1000001
-                AND alochdr.datetrx <= ?
-            GROUP BY alocln.c_invoice_id
-        ),
-        invoice_data AS (
-            SELECT
-                inv.c_invoice_id,
-                inv.totallines,
-                inv.dateinvoiced,
-                org.name as branch_name,
-                COALESCE(ps.bayar, 0) as bayar,
-                CASE
-                    WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NDC') THEN 1
-                    ELSE -1
-                END as pengali,
-                (CURRENT_DATE - inv.dateinvoiced::date) as age
-            FROM c_invoice inv
-            INNER JOIN c_bpartner bp ON inv.c_bpartner_id = bp.c_bpartner_id
-            INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-            LEFT JOIN payment_summary ps ON ps.c_invoice_id = inv.c_invoice_id
-            WHERE SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NCC', 'CNC', 'NDC')
-                AND inv.isactive = 'Y'
-                AND inv.ad_client_id = 1000001
-                AND bp.isactive = 'Y'
-                AND inv.issotrx = 'Y'
-                AND inv.docstatus IN ('CO', 'CL')
-                AND bp.iscustomer = 'Y'
-                AND inv.dateinvoiced <= ?
-                AND inv.c_bpartner_id IS NOT NULL
-                AND inv.totallines IS NOT NULL
-        )
         SELECT
             branch_name,
             SUM(CASE WHEN age >= 1 AND age <= 30 AND (totallines - (bayar * pengali)) <> 0
@@ -444,7 +394,39 @@ class AccountsReceivableController extends Controller
                 THEN (totallines - (bayar * pengali)) ELSE 0 END) as overdue_90_plus,
             SUM(CASE WHEN age >= 1 AND (totallines - (bayar * pengali)) <> 0
                 THEN (totallines - (bayar * pengali)) ELSE 0 END) as total_overdue
-        FROM invoice_data
+        FROM (
+            SELECT
+                inv.totallines,
+                org.name as branch_name,
+                -- Correlated subquery to get payment per invoice
+                (
+                    SELECT COALESCE(SUM(alocln.amount + alocln.writeoffamt + alocln.discountamt), 0)
+                    FROM c_allocationline alocln
+                    INNER JOIN c_allocationhdr alochdr ON alocln.c_allocationhdr_id = alochdr.c_allocationhdr_id
+                    WHERE alocln.c_invoice_id = inv.c_invoice_id
+                        AND alochdr.docstatus IN ('CO', 'IN')
+                        AND alochdr.ad_client_id = 1000001
+                        AND alochdr.datetrx <= ?
+                ) as bayar,
+                CASE
+                    WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NDC') THEN 1
+                    ELSE -1
+                END as pengali,
+                (CURRENT_DATE - inv.dateinvoiced::date) as age
+            FROM c_invoice inv
+            INNER JOIN c_bpartner bp ON inv.c_bpartner_id = bp.c_bpartner_id
+            INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
+            WHERE SUBSTR(inv.documentno, 1, 3) IN ('INC', 'NCC', 'CNC', 'NDC')
+                AND inv.isactive = 'Y'
+                AND inv.ad_client_id = 1000001
+                AND bp.isactive = 'Y'
+                AND inv.issotrx = 'Y'
+                AND inv.docstatus IN ('CO', 'CL')
+                AND bp.iscustomer = 'Y'
+                AND inv.dateinvoiced <= ?
+                AND inv.c_bpartner_id IS NOT NULL
+                AND inv.totallines IS NOT NULL
+        ) as source
         WHERE (totallines - (bayar * pengali)) <> 0
             AND age >= 1
         GROUP BY branch_name
