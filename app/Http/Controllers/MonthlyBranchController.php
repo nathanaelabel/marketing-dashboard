c83a9c5748216457373caad56e57ca70a4da3c88
Abel <nathancodelab@gmail.com>
2025-12-05 14:50:40 +0700

    feat: replace inline queries with stored procedures

--- app/Http/Controllers/MonthlyBranchController.php
+++ app/Http/Controllers/MonthlyBranchController.php
@@ -168,175 +168,36 @@ class MonthlyBranchController extends Controller
         }
     }
 
+    // Mengambil data revenue bulanan menggunakan Stored Procedure
     private function getMonthlyRevenueData($startDate, $endDate, $category, $branch, $type = 'BRUTO')
     {
+        $startTime = microtime(true);
+        $branchParam = ($branch === 'National') ? null : $branch;
+
         try {
-            try {
-                $driver = DB::connection()->getDriverName();
-                if ($driver === 'pgsql') {
-                    DB::statement("SET statement_timeout = 120000");
-                }
-            } catch (\Exception $e) {
-                Log::debug('Could not set statement timeout', ['error' => $e->getMessage()]);
-            }
-
-            $branchCondition = '';
-            $bindings = [];
-
-            if ($branch !== 'National') {
-                $branchCondition = 'AND org.name = ?';
-                $bindings = [$branch, $startDate, $endDate, $category, $category];
-            } else {
-                $bindings = [$startDate, $endDate, $category, $category];
-            }
-
             if ($type === 'NETTO') {
-                // Netto query - includes returns (CNC documents) as negative values
-                $query = "
-                SELECT
-                    EXTRACT(month FROM inv.dateinvoiced) AS month_number,
-                    COALESCE(SUM(CASE
-                        WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC') THEN invl.linenetamt
-                        WHEN SUBSTR(inv.documentno, 1, 3) IN ('CNC') THEN -invl.linenetamt
-                    END), 0) AS total_revenue
-                FROM
-                    c_invoice inv
-                    INNER JOIN c_invoiceline invl ON inv.c_invoice_id = invl.c_invoice_id
-                    INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-                    INNER JOIN c_bpartner cust ON inv.c_bpartner_id = cust.c_bpartner_id
-                    INNER JOIN m_product p ON invl.m_product_id = p.m_product_id
-                    INNER JOIN m_product_category pc ON p.m_product_category_id = pc.m_product_category_id
-                    LEFT JOIN m_productsubcat psc ON p.m_productsubcat_id = psc.m_productsubcat_id
-                WHERE
-                    inv.ad_client_id = 1000001
-                    AND inv.issotrx = 'Y'
-                    AND invl.qtyinvoiced > 0
-                    AND invl.linenetamt > 0
-                    AND inv.docstatus IN ('CO', 'CL')
-                    AND inv.isactive = 'Y'
-                    {$branchCondition}
-                    AND inv.dateinvoiced::date BETWEEN ? AND ?
-                    AND SUBSTR(inv.documentno, 1, 3) IN ('INC', 'CNC')
-                    AND (
-                        CASE 
-                            WHEN ? = 'MIKA' THEN (
-                                pc.value = 'MIKA' 
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND p.name NOT LIKE '%BOHLAM%'
-                                    AND psc.value = 'MIKA'
-                                )
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND (
-                                        p.name LIKE '%FILTER UDARA%'
-                                        OR p.name LIKE '%SWITCH REM%'
-                                        OR p.name LIKE '%DOP RITING%'
-                                    )
-                                )
-                            )
-                            ELSE pc.name = ?
-                        END
-                    )
-                    AND UPPER(cust.name) NOT LIKE '%KARYAWAN%'
-                GROUP BY
-                    EXTRACT(month FROM inv.dateinvoiced)
-                ORDER BY
-                    month_number
-            ";
+                $results = DB::select(
+                    'SELECT * FROM sp_get_monthly_revenue_netto(?, ?, ?, ?)',
+                    [$startDate, $endDate, $category, $branchParam]
+                );
             } else {
-                // Bruto query - original query (only INC documents)
-                $query = "
-                SELECT
-                    EXTRACT(month FROM inv.dateinvoiced) AS month_number,
-                    COALESCE(SUM(invl.linenetamt), 0) AS total_revenue
-                FROM
-                    c_invoice inv
-                    INNER JOIN c_invoiceline invl ON inv.c_invoice_id = invl.c_invoice_id
-                    INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-                    INNER JOIN c_bpartner cust ON inv.c_bpartner_id = cust.c_bpartner_id
-                    INNER JOIN m_product p ON invl.m_product_id = p.m_product_id
-                    INNER JOIN m_product_category pc ON p.m_product_category_id = pc.m_product_category_id
-                    LEFT JOIN m_productsubcat psc ON p.m_productsubcat_id = psc.m_productsubcat_id
-                WHERE
-                    inv.ad_client_id = 1000001
-                    AND inv.issotrx = 'Y'
-                    AND invl.qtyinvoiced > 0
-                    AND invl.linenetamt > 0
-                    AND inv.docstatus IN ('CO', 'CL')
-                    AND inv.isactive = 'Y'
-                    {$branchCondition}
-                    AND inv.dateinvoiced::date BETWEEN ? AND ?
-                    AND inv.documentno LIKE 'INC%'
-                    AND (
-                        CASE 
-                            WHEN ? = 'MIKA' THEN (
-                                pc.value = 'MIKA' 
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND p.name NOT LIKE '%BOHLAM%'
-                                    AND psc.value = 'MIKA'
-                                )
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND (
-                                        p.name LIKE '%FILTER UDARA%'
-                                        OR p.name LIKE '%SWITCH REM%'
-                                        OR p.name LIKE '%DOP RITING%'
-                                    )
-                                )
-                            )
-                            ELSE pc.name = ?
-                        END
-                    )
-                    AND UPPER(cust.name) NOT LIKE '%KARYAWAN%'
-                GROUP BY
-                    EXTRACT(month FROM inv.dateinvoiced)
-                ORDER BY
-                    month_number
-            ";
+                $results = DB::select(
+                    'SELECT * FROM sp_get_monthly_revenue_bruto(?, ?, ?, ?)',
+                    [$startDate, $endDate, $category, $branchParam]
+                );
             }
 
-            $result = DB::select($query, $bindings);
-
-            try {
-                $driver = DB::connection()->getDriverName();
-                if ($driver === 'pgsql') {
-                    DB::statement("SET statement_timeout = 0");
-                }
-            } catch (\Exception $e) {
-            }
-
-            return $result;
-        } catch (\PDOException $e) {
-            try {
-                $driver = DB::connection()->getDriverName();
-                if ($driver === 'pgsql') {
-                    DB::statement("SET statement_timeout = 0");
-                }
-            } catch (\Exception $resetError) {
-            }
-
-            Log::error('MonthlyBranchController getMonthlyRevenueData PDO error', [
+            $duration = round((microtime(true) - $startTime) * 1000, 2);
+            Log::info("MonthlyBranch: getMonthlyRevenueData ({$type}) took {$duration}ms", [
                 'start_date' => $startDate,
                 'end_date' => $endDate,
                 'category' => $category,
                 'branch' => $branch,
-                'type' => $type,
-                'error' => $e->getMessage(),
-                'code' => $e->getCode()
+                'rows' => count($results)
             ]);
 
-            throw $e;
+            return $results;
         } catch (\Exception $e) {
-            try {
-                $driver = DB::connection()->getDriverName();
-                if ($driver === 'pgsql') {
-                    DB::statement("SET statement_timeout = 0");
-                }
-            } catch (\Exception $resetError) {
-            }
-
             Log::error('MonthlyBranchController getMonthlyRevenueData error', [
                 'start_date' => $startDate,
                 'end_date' => $endDate,
@@ -425,139 +286,28 @@ class MonthlyBranchController extends Controller
         $previousStartDate = $previousYear . '-01-01';
         $previousEndDate = $previousYear . '-12-31';
 
-        try {
-            $driver = DB::connection()->getDriverName();
-            if ($driver === 'pgsql') {
-                DB::statement("SET statement_timeout = 300000"); // 5 minutes
-            }
-        } catch (\Exception $e) {
-            Log::debug('Could not set statement timeout for export', ['error' => $e->getMessage()]);
-        }
+        // Menggunakan Stored Procedure untuk export
+        $startTime = microtime(true);
 
         if ($type === 'NETTO') {
-            // Netto query - includes returns (CNC documents) as negative values
-            $query = "
-                SELECT
-                    org.name AS branch_name,
-                    EXTRACT(month FROM inv.dateinvoiced) AS month_number,
-                    EXTRACT(year FROM inv.dateinvoiced) AS year_number,
-                    COALESCE(SUM(CASE
-                        WHEN SUBSTR(inv.documentno, 1, 3) IN ('INC') THEN invl.linenetamt
-                        WHEN SUBSTR(inv.documentno, 1, 3) IN ('CNC') THEN -invl.linenetamt
-                    END), 0) AS total_revenue
-                FROM
-                    c_invoice inv
-                    INNER JOIN c_invoiceline invl ON inv.c_invoice_id = invl.c_invoice_id
-                    INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-                    INNER JOIN c_bpartner cust ON inv.c_bpartner_id = cust.c_bpartner_id
-                    INNER JOIN m_product p ON invl.m_product_id = p.m_product_id
-                    INNER JOIN m_product_category pc ON p.m_product_category_id = pc.m_product_category_id
-                    LEFT JOIN m_productsubcat psc ON p.m_productsubcat_id = psc.m_productsubcat_id
-                WHERE
-                    inv.ad_client_id = 1000001
-                    AND inv.issotrx = 'Y'
-                    AND invl.qtyinvoiced > 0
-                    AND invl.linenetamt > 0
-                    AND inv.docstatus IN ('CO', 'CL')
-                    AND inv.isactive = 'Y'
-                    AND (
-                        (inv.dateinvoiced::date BETWEEN ? AND ?)
-                        OR (inv.dateinvoiced::date BETWEEN ? AND ?)
-                    )
-                    AND SUBSTR(inv.documentno, 1, 3) IN ('INC', 'CNC')
-                    AND (
-                        CASE 
-                            WHEN ? = 'MIKA' THEN (
-                                pc.value = 'MIKA' 
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND p.name NOT LIKE '%BOHLAM%'
-                                    AND psc.value = 'MIKA'
-                                )
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND (
-                                        p.name LIKE '%FILTER UDARA%'
-                                        OR p.name LIKE '%SWITCH REM%'
-                                        OR p.name LIKE '%DOP RITING%'
-                                    )
-                                )
-                            )
-                            ELSE pc.name = ?
-                        END
-                    )
-                    AND UPPER(cust.name) NOT LIKE '%KARYAWAN%'
-                GROUP BY
-                    org.name, EXTRACT(month FROM inv.dateinvoiced), EXTRACT(year FROM inv.dateinvoiced)
-                ORDER BY
-                    org.name, year_number, month_number
-            ";
+            $allData = DB::select(
+                'SELECT * FROM sp_get_monthly_revenue_export_netto(?, ?, ?, ?, ?)',
+                [$previousStartDate, $previousEndDate, $startDate, $endDate, $category]
+            );
         } else {
-            // Bruto query - original query (only INC documents)
-            $query = "
-                SELECT
-                    org.name AS branch_name,
-                    EXTRACT(month FROM inv.dateinvoiced) AS month_number,
-                    EXTRACT(year FROM inv.dateinvoiced) AS year_number,
-                    COALESCE(SUM(invl.linenetamt), 0) AS total_revenue
-                FROM
-                    c_invoice inv
-                    INNER JOIN c_invoiceline invl ON inv.c_invoice_id = invl.c_invoice_id
-                    INNER JOIN ad_org org ON inv.ad_org_id = org.ad_org_id
-                    INNER JOIN c_bpartner cust ON inv.c_bpartner_id = cust.c_bpartner_id
-                    INNER JOIN m_product p ON invl.m_product_id = p.m_product_id
-                    INNER JOIN m_product_category pc ON p.m_product_category_id = pc.m_product_category_id
-                    LEFT JOIN m_productsubcat psc ON p.m_productsubcat_id = psc.m_productsubcat_id
-                WHERE
-                    inv.ad_client_id = 1000001
-                    AND inv.issotrx = 'Y'
-                    AND invl.qtyinvoiced > 0
-                    AND invl.linenetamt > 0
-                    AND inv.docstatus IN ('CO', 'CL')
-                    AND inv.isactive = 'Y'
-                    AND (
-                        (inv.dateinvoiced::date BETWEEN ? AND ?)
-                        OR (inv.dateinvoiced::date BETWEEN ? AND ?)
-                    )
-                    AND inv.documentno LIKE 'INC%'
-                    AND (
-                        CASE 
-                            WHEN ? = 'MIKA' THEN (
-                                pc.value = 'MIKA' 
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND p.name NOT LIKE '%BOHLAM%'
-                                    AND psc.value = 'MIKA'
-                                )
-                                OR (
-                                    pc.value = 'PRODUCT IMPORT' 
-                                    AND (
-                                        p.name LIKE '%FILTER UDARA%'
-                                        OR p.name LIKE '%SWITCH REM%'
-                                        OR p.name LIKE '%DOP RITING%'
-                                    )
-                                )
-                            )
-                            ELSE pc.name = ?
-                        END
-                    )
-                    AND UPPER(cust.name) NOT LIKE '%KARYAWAN%'
-                GROUP BY
-                    org.name, EXTRACT(month FROM inv.dateinvoiced), EXTRACT(year FROM inv.dateinvoiced)
-                ORDER BY
-                    org.name, year_number, month_number
-            ";
+            $allData = DB::select(
+                'SELECT * FROM sp_get_monthly_revenue_export_bruto(?, ?, ?, ?, ?)',
+                [$previousStartDate, $previousEndDate, $startDate, $endDate, $category]
+            );
         }
 
-        $allData = DB::select($query, [$previousStartDate, $previousEndDate, $startDate, $endDate, $category, $category]);
-
-        try {
-            $driver = DB::connection()->getDriverName();
-            if ($driver === 'pgsql') {
-                DB::statement("SET statement_timeout = 0");
-            }
-        } catch (\Exception $e) {
-        }
+        $duration = round((microtime(true) - $startTime) * 1000, 2);
+        Log::info("MonthlyBranch: exportExcel ({$type}) query took {$duration}ms", [
+            'prev_period' => "{$previousStartDate} - {$previousEndDate}",
+            'curr_period' => "{$startDate} - {$endDate}",
+            'category' => $category,
+            'rows' => count($allData)
+        ]);
 
         $allData = ChartHelper::sortByBranchOrder(collect($allData), 'branch_name')->all();
 
@@ -844,28 +594,12 @@ class MonthlyBranchController extends Controller
 
         $previousYear = $year - 1;
 
-        try {
-            $driver = DB::connection()->getDriverName();
-            if ($driver === 'pgsql') {
-                DB::statement("SET statement_timeout = 300000"); // 5 minutes
-            }
-        } catch (\Exception $e) {
-            Log::debug('Could not set statement timeout for export', ['error' => $e->getMessage()]);
-        }
-
+        // Menggunakan Stored Procedure via getMonthlyRevenueData
         $currentYearData = $this->getMonthlyRevenueData($startDate, $endDate, $category, $branch, $type);
 
         $previousStartDate = $previousYear . '-01-01';
         $previousEndDate = $previousYear . '-12-31';
         $previousYearData = $this->getMonthlyRevenueData($previousStartDate, $previousEndDate, $category, $branch, $type);
-
-        try {
-            $driver = DB::connection()->getDriverName();
-            if ($driver === 'pgsql') {
-                DB::statement("SET statement_timeout = 0");
-            }
-        } catch (\Exception $e) {
-        }
 
         $monthLabels = [
             'January',
